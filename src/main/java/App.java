d670ac7fa11e824e60de84a751f510a8a7a1a38c
Arseny <senyanyash64@gmail.com>
2023-01-03 14:24:33 +0300

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat1.equals(cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String name;
+
+    public Cat() {
+
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
